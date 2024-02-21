7c540508f622e1978afb21ef7da90984b158a50e
Rodrigo Dalmas <rodrigo.dalmas@sosafe.de>
2024-02-21 12:12:01 +0000

    Basic navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HomePage, ShopPage, ProductsPage, ProductPage, NotFoundPage } from './pages';
+import { Layout } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="shop" element={<ShopPage />} />
+          <Route path="products" element={<ProductsPage />}>
+            <Route path=":id" element={<ProductPage />} />
+          </Route>
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import HomePage from "./Home";
+import ShopPage from "./Shop";
+import ProductsPage from "./Products";
+import ProductPage from "./Product";
+import NotFoundPage from "./NotFound";
+
+export { HomePage, ShopPage, ProductsPage, ProductPage, NotFoundPage };
