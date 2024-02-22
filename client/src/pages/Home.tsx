1515a50deea29f0d22ba34e4a3ba75871738acf6
Rodrigo Dalmas <rodrigo.dalmas@sosafe.de>
2024-02-22 16:34:40 +0000

    Implemented Home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import homeVideo1 from '../assets/homeVideo1.mp4';
+
+import './Home.scss';
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className='main'>
+      <div className='overlay' />
+      <video src={homeVideo1} autoPlay loop muted></video>
+      <div className='content'>
+        <div>
+          <h2>Monogram</h2>
+          <h1>Creative Console</h1>
+          <h4>Simple. Powerful. Adaptable. A better way to create</h4>
+        </div>
+      </div>
     </div>
   );
 }
