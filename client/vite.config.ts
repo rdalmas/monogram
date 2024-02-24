1af0538cb02f3b21eb1306ee8130ab642bd3bb4b
Rodrigo Dalmas <rodrigo.dalmas@sosafe.de>
2024-02-24 19:49:35 +0000

    Configuring CSS modules on Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sass from 'sass';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: sass
+      },
+    },
+    modules: {
+      scopeBehaviour: 'local',
+      hashPrefix: 'hash',
+      localsConvention: 'dashesOnly'
+    }
+  },
   server: {
     port: 3000,
     host: true,
   }
-})
+});
